fc784d06ccf4ec71b5aa451ffefc47718aeea1fd
bjoonly <elumnaya@gmail.com>
2020-11-30 21:55:39 +0200

    Init

---
+++ TextEditor/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace TextEditor
+{
+    static public class Class1
+    {
+        public static TextPointer GetTextPointerAtOffset(this RichTextBox richTextBox, int offset)
+        {
+            TextPointer navigator = richTextBox.Document.ContentStart;
+            int count = 0;
+
+            while (navigator.CompareTo(richTextBox.Document.ContentEnd) < 0)
+            {
+                switch (navigator.GetPointerContext(LogicalDirection.Forward))
+                {
+                    case TextPointerContext.ElementStart:
+                        break;
+                    case TextPointerContext.ElementEnd:
+                        if (navigator.GetAdjacentElement(LogicalDirection.Forward) is Paragraph)
+                            count += 2;
+                        break;
+                    case TextPointerContext.EmbeddedElement:
+                        count++;
+                        break;
+                    case TextPointerContext.Text:
+                        int runLength = navigator.GetTextRunLength(LogicalDirection.Forward);
+
+                        if (runLength > 0 && runLength + count < offset)
+                        {
+                            count += runLength;
+                            navigator = navigator.GetPositionAtOffset(runLength);
+                            if (count > offset)
+                                break;
+                            continue;
+                        }
+                        count++;
+                        break;
+                }
+
+                if (count > offset)
+                    break;
+
+                navigator = navigator.GetPositionAtOffset(1, LogicalDirection.Forward);
+
+            } 
+
+            return navigator;
+        }
+    }
+}
---
+++ TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        int startPos=0;
+        int endPos=1;
+        private TextPointer textPointerStart;
+        private TextPointer textPointerEnd;
+        public MainWindow()
+        {
+            InitializeComponent();
+            textPointerStart = richTextBox.GetTextPointerAtOffset(startPos); 
+            textPointerEnd = richTextBox.GetTextPointerAtOffset(endPos);
+
+        }
+       
+        private int GetLenght()
+        {
+            TextRange content = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            return content.Text.Length;
+        }
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+    
+         
+           GetSelectionText().ApplyPropertyValue(Run.FontWeightProperty,FontWeights.Bold);
+        }
+        public void MakeInactive()
+        {
+            boldButton.IsEnabled = false;
+            italicButton.IsEnabled = false;
+            underlineButton.IsEnabled = false;
+            clearButton.IsEnabled = false;
+            comboBoxSize.IsEnabled = false;
+            comboBoxColor.IsEnabled = false;
+        }
+        public void MakeActive()
+        {
+            boldButton.IsEnabled = true;
+            italicButton.IsEnabled = true;
+            underlineButton.IsEnabled = true;
+            clearButton.IsEnabled = true;
+            comboBoxSize.IsEnabled = true;
+            comboBoxColor.IsEnabled = true;
+        }
+
+        private void TextBoxStart_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (paragraph == null)
+                return;
+            
+            string line = textBoxStart.Text;
+
+            if (int.TryParse(line, out int startPos) && startPos>=0 && startPos<=endPos-1)
+            {
+                this.startPos = startPos;
+                MakeActive();
+            }
+            else if(startPos>endPos-1)
+            {
+                this.startPos = startPos;
+                MakeInactive();                            
+            }
+            else
+            {
+                MakeInactive();
+            }
+
+        }
+
+        private void TextBoxEnd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (paragraph == null)
+                return;
+
+            string line = textBoxEnd.Text.Trim();
+            if ( int.TryParse(line, out int endPos) && endPos >= startPos &&  endPos <GetLenght()-1)
+            {
+                this.endPos = endPos+1;
+                MakeActive();
+            }
+            else if(endPos < startPos)
+            {
+                this.endPos = endPos + 1;
+                MakeInactive();              
+            }
+            else
+            {
+                MakeInactive();
+            }
+        }
+
+
+        private TextSelection GetSelectionText()
+        {
+            textPointerStart = richTextBox.GetTextPointerAtOffset(startPos);
+            textPointerEnd = richTextBox.GetTextPointerAtOffset(endPos);  
+            richTextBox.Selection.Select(textPointerStart, textPointerEnd);         
+            return richTextBox.Selection;
+        }
+        private void italicButton_Click(object sender, RoutedEventArgs e)
+        {
+         
+            GetSelectionText().ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Italic);
+        }
+
+        private void underlineButton_Click(object sender, RoutedEventArgs e)
+        {
+         
+            GetSelectionText().ApplyPropertyValue(Run.TextDecorationsProperty, TextDecorations.Underline);
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextSelection text = GetSelectionText();
+            text.ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Normal);
+            text.ApplyPropertyValue(Run.TextDecorationsProperty, null);
+            text.ApplyPropertyValue(Run.FontWeightProperty, FontWeights.Normal);
+            text.ApplyPropertyValue(RichTextBox.FontSizeProperty, 16.0);
+            text.ApplyPropertyValue(Run.ForegroundProperty, new SolidColorBrush( Colors.Black));
+       
+        }
+
+        private void comboBoxSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (paragraph != null && comboBoxSize.SelectedItem != null)
+            {
+             
+                double value = double.Parse(((ComboBoxItem)comboBoxSize.SelectedItem).Content.ToString());
+                GetSelectionText().ApplyPropertyValue(RichTextBox.FontSizeProperty,value);
+            }
+        }
+
+        private void comboBoxColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBoxColor.SelectedItem != null && paragraph!=null)
+            {
+               Color color=(Color)( ColorConverter.ConvertFromString(((ComboBoxItem)comboBoxColor.SelectedItem).Tag.ToString()));
+                 GetSelectionText().ApplyPropertyValue(Run.ForegroundProperty,new SolidColorBrush(color));
+            }
+        }
+    }
+    }
+
